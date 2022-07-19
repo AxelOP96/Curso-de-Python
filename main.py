fd350630a7130cf90aff72b22b91d10330e918c4
Axel Daniel <77642568+AxelOP96@users.noreply.github.com>
2022-07-19 21:35:45 +0000

    proyecto que crea un nombre de banda

---
+++ main.py
@@ -0,0 +1,3 @@
+#Imprime la oracion hola mundo
+#print("Hello world")
+print("Welcome to the Band name generator!\nWhat' s the city wher you grew in?")
---
+++ proyecto1.py
@@ -0,0 +1,7 @@
+print("Welcome to the Band name generator!\n)
+
+print(What' s the city wher you grew in?\n")
+name = input()
+print("What' s the name of a pet?\n")  
+pet = input()
+print("Your band name could be " + name + " " + pet)      
